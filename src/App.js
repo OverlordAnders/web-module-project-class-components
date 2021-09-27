f7966d4b5bba52fa17a3786152953d315486d259
Brina <53455199+OverlordAnders@users.noreply.github.com>
2021-09-27 18:01:40 -0400

    Fleshed out the todo list
    So I got the general setup for the todoList but not sure why it's not clearing the todo list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,76 @@
 import React from 'react';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+const items = [
+{
+  name:'forks',
+  id: 123,
+  purchased: false,
+
+}
+ 
+
+]
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(){
+    super();
+    this.state = {
+      items: items
+    }
+  }
+
+  toggleItem = (id) => {
+    const newItems = this.state.items.map(item => {
+      if(item.id === id){
+        return({
+          ...item,
+          purchased:!item.purchased
+        })
+      }
+      else{
+        return item
+      }
+    });
+   
+    this.setState({
+      items: newItems
+    });
+  }
+  
+  addItem = (itemName)=> {
+    this.setState({
+      items:[...this.state.items, {
+        name:itemName,
+        id: this.state.length,
+        purchased: false
+      }]
+    });
+  }
+
+ clearPurchased = e => {
+   this.setState({
+     items: this.state.items.filter(item => {
+      return ( item.purchased === false)
+     })
+   });
+ }
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+       <div>
+        <h1>Todo List</h1>
+        <TodoForm addItem={this.addItem} clearPurchased={this.clearPurchased} />
       </div>
+      <TodoList toggleItem={this.toggleItem} items={this.state.items} />
+    </div>      
     );
+
   }
 }
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,18 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react";
+import Item from "./Todo";
+
+
+const TodoList = props => {
+    return(
+        <div className="shopping-list">
+            {props.items.map(item => (
+                <Item toggleItem={props.toggleItem} key={item.id} item={item} />
+            ))}
+
+        </div>
+    )
+}
+
+export default TodoList;
